4567201213ce11d12d8aa40d51cb6968908bb1d4
Rachel <Raquel.rudermano@gmail.com>
2022-08-22 10:25:10 -0700

    add deps

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('Url check', ()=>{
+  it('Url load check', ()=>{
+
+//         URL: https://prodtest.demosphere-secure.com/_/_officials-mgmt 
+//                         OR
+//              https://prodtest.demosphere-secure.com/_dashboard
+
+      cy.visit('https://prodtest.demosphere-secure.com/_/_officials-mgmt')        
+     
+      cy.get('#user_login').type('alidem2')
+      cy.get('#user_password').type('Alidem2')
+      cy.get('[type="Submit"]').click()
+   
   })
+
 })
