6693c2612ed97aeece258a1ba0c2ceaabf0573e6
Rachel <Raquel.rudermano@gmail.com>
2023-07-24 16:50:17 -0400

    upgrade cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,17 +2,6 @@
 
 describe('Url check', ()=>{
   it('Url load check', ()=>{
-
-//         URL: https://prodtest.demosphere-secure.com/_/_officials-mgmt 
-//                         OR
-//              https://prodtest.demosphere-secure.com/_dashboard
-
-      cy.visit('https://prodtest.demosphere-secure.com/_/_officials-mgmt')        
-     
-      cy.get('#user_login').type('alidem2')
-      cy.get('#user_password').type('Alidem2')
-      cy.get('[type="Submit"]').click()
-   
+      cy.visit('https://www.google.com')
   })
-
 })
